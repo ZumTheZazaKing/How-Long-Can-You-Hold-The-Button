d2f58a53dbee45e198719099d55a7a40d3fcc8fd
Purogurama Z <73376015+ZumTheZazaKing@users.noreply.github.com>
2021-01-15 00:20:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let bestScore = document.getElementById('best');
+
+let timer = document.getElementById('timer');
+
+let playerScore = document.getElementById('results');
+
+let playeScoreContainer = document.getElementById('score');
+
+let playAgainButton = document.getElementById('playAgain')
+
+bestScore.innerHTML = localStorage.getItem('best');
+
+let counter = 0;
+
+function setTimer(){
+
+
+    counter++;
+
+    timer.innerHTML = counter;
+
+}
+
+function hold(){
+
+    setInterval(setTimer,1000);
+
+}
+
+function offHold(){
+
+    button.style.display = 'none';
+
+    playerScore.innerHTML = timer.innerHTML;
+
+    playeScoreContainer.classList.remove('hide');
+
+    playAgainButton.classList.remove('hide');
+
+    if (parseInt(playerScore.innerHTML) > parseInt(bestScore.innerHTML)){
+
+        localStorage.setItem('best',playerScore.innerHTML);
+
+        bestScore.innerHTML = localStorage.getItem('best');
+
+    }
+
+
+}
+
+function reload(){
+
+    window.location.reload();
+
+}
